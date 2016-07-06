b17a8968a65e8d449c96b1ce4cc9309771d7769c
HashZhang <zhanggaoxin@sf-express.com>
2016-07-06 12:28:45 +0800

    1. 增加更多单元测试

--- collecat-manager/src/test/java/com/sf/collecat/manager/db/BaseTest.java
+++ collecat-manager/src/test/java/com/sf/collecat/manager/db/BaseTest.java
@@ -1,7 +1,13 @@
 package com.sf.collecat.manager.db;
 
+import com.sf.collecat.common.Constants;
+import com.sf.collecat.common.mapper.JobMapper;
 import com.sf.collecat.common.mapper.NodeMapper;
 import com.sf.collecat.common.mapper.TaskMapper;
+import com.sf.collecat.common.model.Job;
+import com.sf.collecat.common.model.Node;
+import com.sf.collecat.common.model.Task;
+import junit.framework.Assert;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
@@ -19,9 +25,48 @@ public class BaseTest extends AbstractJUnit4SpringContextTests {
     NodeMapper nodeMapper;
     @Autowired
     TaskMapper taskMapper;
+    @Autowired
+    JobMapper jobMapper;
 
     @Test
-    public void testBillKind() {
+    public void testNode() {
+        Node node = new Node();
+        System.out.println(nodeMapper.insert(node));
+        System.out.println(nodeMapper.selectAll());
+        node.setIp("10.202.4.39");
+        System.out.println(nodeMapper.updateByPrimaryKey(node));
+        System.out.println(nodeMapper.selectAll());
+        Node node2 = nodeMapper.selectByPrimaryKey(node.getId());
+        Assert.assertEquals(node.getIp(),node2.getIp());
+        System.out.println(nodeMapper.deleteByPrimaryKey(node.getId()));
+        System.out.println(nodeMapper.selectAll());
+    }
+
+    @Test
+    public void testTask() {
+        Task task = new Task();
+        System.out.println(taskMapper.insert(task));
         System.out.println(taskMapper.selectAll());
+        task.setInitialSql("select * from aaaa");
+        System.out.println(taskMapper.updateByPrimaryKey(task));
+        System.out.println(taskMapper.selectAll());
+        Task task2 = taskMapper.selectByPrimaryKey(task.getId());
+        Assert.assertEquals(task.getInitialSql(),task2.getInitialSql());
+        System.out.println(taskMapper.deleteByPrimaryKey(task.getId()));
+        System.out.println(taskMapper.selectAll());
+    }
+
+    @Test
+    public void testJob() {
+        Job job = new Job();
+        System.out.println(jobMapper.insert(job));
+        System.out.println(jobMapper.selectAllJob());
+        job.setStatus(Constants.JOB_EXCEPTION_VALUE);
+        System.out.println(jobMapper.updateByPrimaryKey(job));
+        Job job2 = jobMapper.selectByPrimaryKey(job.getId());
+        Assert.assertEquals(job.getStatus(),job2.getStatus());
+        System.out.println(jobMapper.selectAllExceptionJob());
+        System.out.println(jobMapper.deleteByPrimaryKey(job.getId()));
+        System.out.println(jobMapper.selectAllJob());
     }
 }
